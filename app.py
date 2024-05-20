29cb957ac8384f25f5a54ccf485bdf43e5da0baf
bradx32 <bradx32@hotmail.com>
2024-05-20 22:25:16 +1000

    app with 3 routes

--- app.py
+++ app.py
@@ -4,4 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+     return "<p>Hello, World!</p>"
+
+@app.route("/goodbye/")
+def goodbye_world():
+    return "<p>Goodbye, World!</p>"
+
+@app.route("/coder/")
+def coder():
+    return "<p>This web app was created in a class at Coder Academy.</p>"
